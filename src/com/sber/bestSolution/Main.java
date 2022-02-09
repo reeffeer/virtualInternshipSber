52d3680a796ea98895ff95fa88ff8097fa614b56
Oleg <reefshark@list.ru>
2022-02-09 18:22:34 +0300

    different types of searching
    added lambda and searching via insertion sort

--- src/com/sber/bestSolution/Main.java
+++ src/com/sber/bestSolution/Main.java
@@ -1,6 +1,7 @@
 package com.sber.bestSolution;
 
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -25,17 +26,20 @@ public class Main {
 
         //вариант с сортировкой по населению
 //        sortPopulation(cities);
-        // отсортировали список в порядке убывания населения, на первой позиции будет город с наибольшим населением
-//        print(cities);
-        //перевели в массив
+//        // отсортировали список в порядке убывания населения, на первой позиции будет город с наибольшим населением
+////        print(cities);
+//        //перевели в массив
 //        toArr(cities);
-        //нулевой индекс массива будет соответсвтовать городу с максимальным населением
+//        //нулевой индекс массива будет соответсвтовать городу с максимальным населением
 //        System.out.println("---City with the biggest population---");
 //        System.out.println(cities.get(0));
 
         //вариант без сортировки
-        findBiggestPopulation(cities);
+//        simpleFindBiggestPopulation(cities);
 
+//        findByInsertionSort(cities);
+
+        lambdaFindMaxPopulation(cities);
     }
 
     public static void toArr(List<City> cities) {
@@ -51,17 +55,35 @@ public class Main {
         }.reversed());
     }
 
-    public static void findBiggestPopulation(List<City> cities) {
-        City[] citiesArray = cities.toArray(new City[0]);
-        City[] citiesArr = citiesArray;
-        int maxPop = citiesArr[0].getPopulation();
+    public static void simpleFindBiggestPopulation(List<City> cities) {
+        City[] citiesArr = cities.toArray(new City[cities.size()]);
+        City maxPop = citiesArr[0];
         int indexOfMaxPop = 0;
         for (int i = 1; i < citiesArr.length; i++) {
-            if (citiesArr[i].getPopulation() > maxPop) {
-                maxPop = citiesArr[i].getPopulation();
+            if (citiesArr[i].getPopulation() > maxPop.getPopulation()) {
+                maxPop = citiesArr[i];
                 indexOfMaxPop = i;
             }
         }
-        System.out.println("[" + indexOfMaxPop +"]" + " = " + maxPop);
+        System.out.println("[" + indexOfMaxPop +"]" + " = " + maxPop.getPopulation());
+    }
+
+    public static void findByInsertionSort(List<City> cities) {
+        City[] arr = new City[cities.size()];
+        cities.toArray(arr);
+        for (int i = 1; i < arr.length; i++) {
+            City current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current.getPopulation() < arr[j].getPopulation()) {
+             arr[j + 1] = arr[j];
+             j--;
+            }
+            arr[j + 1] = current;
+        }
+        System.out.println("[" + (arr.length-1) + "]" + " = " + arr[arr.length-1].getPopulation());
+    }
+
+    public static void lambdaFindMaxPopulation(List<City> cities) {
+        System.out.println(cities.stream().max(Comparator.comparing(City::getPopulation)));
     }
 }
